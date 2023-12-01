a7d909a2ff418e85318d90945475094a5ab2c26d
ZiadAhmed92 <za693387@gmail.com>
2023-12-01 10:59:28 +0200

    redux-persist

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,18 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import "../styles/Header.css"
 import { Provider } from 'react-redux'
 import { persistor, store } from '../store/store'
-import { PersistGate } from "redux-persist/integration/react";
+// import { PersistGate } from "redux-persist/integration/react";
 export default function App({ Component, pageProps }) {
   return (<>
 
   <Provider store={store}>
-  <PersistGate persistor={persistor} loading={null}>
+  {/* <PersistGate persistor={persistor} loading={null}> */}
   <div className='color' >
      <Layout>
        <Component {...pageProps} />
      </Layout>
    </div >
-   </PersistGate>
+   {/* </PersistGate> */}
   </Provider>
   </>)
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,33 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit'
 import itemsSliceReducer from './nextSlice'
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
 
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
+// const persistConfig = {
+//   key: "root",
+//   version: 1,
+//   storage,
+// };
 
-const persistedReducer = persistReducer(persistConfig, itemsSliceReducer);
+// const persistedReducer = persistReducer(persistConfig, itemsSliceReducer);
 export const store = configureStore({
   reducer: {
-    items:persistedReducer,
+    items:itemsSliceReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+    // }),
 })
-export let persistor = persistStore(store);
+// export let persistor = persistStore(store);
