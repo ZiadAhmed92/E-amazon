5b9451e7a4b0b95f12d66b7843c226d0eb1962ef
ZiadAhmed92 <za693387@gmail.com>
2023-12-06 14:29:14 +0200

    add PersistGate

--- src/Components/CartPayment.jsx
+++ src/Components/CartPayment.jsx
@@ -3,47 +3,57 @@ import { GiDandelionFlower } from "react-icons/gi";
 import FormaterPrice from './FormaterPrice';
 import { useDispatch, useSelector } from 'react-redux';
 const CartPayment = () => {
-    const [sum, setSum] = useState(0)
-    const { cartProducts } = useSelector((state) => state.items)
-    const dispatch = useDispatch();
-  
-  
-  
-  
-    useEffect(() => {
-      setSum(0)
-      cartProducts.forEach(({ quantity, price }) => {
-        const totalForItem = quantity * price;
-        setSum((state) => state + totalForItem)
-      })
-    }, [cartProducts, dispatch])  
+  const [sum, setSum] = useState(0)
+  const { cartProducts, userInfo } = useSelector((state) => state.items)
+  const dispatch = useDispatch();
+
+
+
+
+  useEffect(() => {
+    setSum(0)
+    cartProducts.forEach(({ quantity, price }) => {
+      const totalForItem = quantity * price;
+      setSum((state) => state + totalForItem)
+    })
+  }, [cartProducts, dispatch])
   return (
     <div className='col-md-3 '>
-    <div className='bg-white container-crad p-3'>
-      <div className='d-flex gap-3'>
-        <div><GiDandelionFlower className="icon-crad" /></div>
-        <p>
-          Your order qualifies for FREE Shipping by Choosing this option at
-          checkout. See details....
-        </p>
+      <div className='bg-white container-crad p-3'>
+        <div className='d-flex gap-3'>
+          <div><GiDandelionFlower className="icon-crad" /></div>
+          <p>
+            Your order qualifies for FREE Shipping by Choosing this option at
+            checkout. See details....
+          </p>
+        </div>
+        <span className="">
+          <p className='fw-bolder fs-5 d-flex gap-3' >Total: <FormaterPrice amount={sum} /></p>
+
+        </span>
+        {
+          userInfo ? <button
+
+            className="btn btn-info w-100 fw-bolder text-white"
+          >
+            Proceed to Pay
+          </button>: 
+          
+         <> <button
+         className="btn btn-info cursor_notAllow w-100 fw-bolder text-white "
+       >
+         Proceed to Pay
+       </button>
+       <p className="mx-auto pt-2 text-center text-danger bouncing-element">
+       Please login to continue
+     </p></>
+        }
+
+
+        
+
       </div>
-      <span className="">
-        <p className='fw-bolder fs-5 d-flex gap-3' >Total: <FormaterPrice amount={sum}/></p>
-
-      </span>
-      <button
-
-        className="btn btn-info w-100 fw-bolder text-white"
-      >
-        Proceed to Pay
-      </button>
-
-      <p className="mx-auto text-center text-danger bouncing-element">
-        Please login to continue
-      </p>
-
     </div>
-  </div>
   )
 }
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,14 +3,14 @@ import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "../styles/Header.css"
 import { Provider } from 'react-redux'
-import { store } from '../store/store'
-// import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from '../store/store'
+import { PersistGate } from "redux-persist/integration/react";
 import { SessionProvider } from "next-auth/react"
 export default function App({ Component,  pageProps: { session, ...pageProps } }) {
   return (<>
 
   <Provider store={store}>
-  {/* <PersistGate persistor={persistor} loading={null}> */}
+  <PersistGate persistor={persistor} loading={null}>
  
  <SessionProvider  session={session}>
   <div className='color' >
@@ -20,7 +20,7 @@ export default function App({ Component,  pageProps: { session, ...pageProps } }
    </div >
  </SessionProvider>
   
-   {/* </PersistGate> */}
+   </PersistGate>
   </Provider>
   </>)
 }
